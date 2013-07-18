c29aef745bb6b8cc6992b814ef72ada18d5e89a0
Brendan Long <b.long@cablelabs.com>
2013-07-18 13:33:20 -0600

    Update license to BSD.

--- src/rui_dtcpip.h
+++ src/rui_dtcpip.h
@@ -1,33 +1,27 @@
-// COPYRIGHT_BEGIN
-//  DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
-//
-//  Copyright (C) 2013 Cable Television Laboratories, Inc.
-//
-//  This program is free software; you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, version 2. This program is distributed
-//  in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
-//  even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-//  PURPOSE. See the GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License along
-//  with this program.  If not, see  <http://www.gnu.org/licenses/>.
-//
-//  Please contact CableLabs if you need additional information or
-//  have any questions.
-//  #include <gst/gst.h>
-
-//      CableLabs
-//      858 Coal Creek Cir
-//      Louisville, CO 80027-9750
-//      303 661-9100
-//      oc-mail@cablelabs.com
-//
-//  If you or the company you represent has a separate agreement with CableLabs
-//  concerning the use of this code, your rights and obligations with respect
-//  to this code shall be as set forth therein. No license is granted hereunder
-//  for any other purpose.
-// COPYRIGHT_END
+/* Copyright (C) 2013 Cable Television Laboratories, Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS
+ * IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #ifndef _RUI_DTCPIP_H_
 #define _RUI_DTCPIP_H_
